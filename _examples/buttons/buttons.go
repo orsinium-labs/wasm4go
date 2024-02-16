7d4e8301edf93f6f8cef47d34af3da8f03381524
gram <git@orsinium.dev>
2024-02-16 18:06:17 +0100

    show all gamepads

--- _examples/buttons/buttons.go
+++ _examples/buttons/buttons.go
@@ -8,23 +8,40 @@ func main() {
 
 func update() {
 	var offsetX uint8 = 60
-	var offsetY uint8 = 72
-	if w4.Gamepads[0].X() {
-		w4.DrawText("\x80", w4.Point{X: offsetX, Y: offsetY})
+	for i, g := range w4.Gamepads {
+		var offsetY uint8 = uint8(72 + i*10)
+		w4.DrawText(itoa(i), w4.Point{X: offsetX, Y: offsetY})
+		if g.X() {
+			w4.DrawText("\x80", w4.Point{X: offsetX + 8, Y: offsetY})
+		}
+		if g.Z() {
+			w4.DrawText("\x81", w4.Point{X: offsetX + 16, Y: offsetY})
+		}
+		if g.Left() {
+			w4.DrawText("\x84", w4.Point{X: offsetX + 24, Y: offsetY})
+		}
+		if g.Right() {
+			w4.DrawText("\x85", w4.Point{X: offsetX + 32, Y: offsetY})
+		}
+		if g.Up() {
+			w4.DrawText("\x86", w4.Point{X: offsetX + 38, Y: offsetY})
+		}
+		if g.Down() {
+			w4.DrawText("\x87", w4.Point{X: offsetX + 46, Y: offsetY})
+		}
 	}
-	if w4.Gamepads[0].Z() {
-		w4.DrawText("\x81", w4.Point{X: offsetX + 8, Y: offsetY})
+}
+
+func itoa(i int) string {
+	switch i {
+	case 0:
+		return "1"
+	case 1:
+		return "2"
+	case 2:
+		return "3"
+	case 3:
+		return "4"
 	}
-	if w4.Gamepads[0].Left() {
-		w4.DrawText("\x84", w4.Point{X: offsetX + 16, Y: offsetY})
-	}
-	if w4.Gamepads[0].Right() {
-		w4.DrawText("\x85", w4.Point{X: offsetX + 24, Y: offsetY})
-	}
-	if w4.Gamepads[0].Up() {
-		w4.DrawText("\x86", w4.Point{X: offsetX + 32, Y: offsetY})
-	}
-	if w4.Gamepads[0].Down() {
-		w4.DrawText("\x87", w4.Point{X: offsetX + 38, Y: offsetY})
-	}
+	panic("gamepad out of range")
 }
