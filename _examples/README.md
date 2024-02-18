b5a7900e56abf7b2edbd6a95cf2fec1565a59498
gram <git@orsinium.dev>
2024-02-18 09:51:15 +0100

    fix snake collision with the screen side

--- _examples/README.md
+++ _examples/README.md
@@ -6,4 +6,5 @@ Available examples:
 
 * [hello](./hello/hello.go): the same code example as wasm-4 CLI generates by default.
 * [buttons](./buttons/buttons.go): displays on the screen the button you press.
+* [snake](./snake/snake.go): re-implementation of the [Snake tutorial](https://wasm4.org/docs/tutorials/snake/goal) from thw official WASM-4 documentation.
 * [tests](./tests/tests.go): integration tests for the project.
--- _examples/snake/snake.go
+++ _examples/snake/snake.go
@@ -6,12 +6,19 @@ import (
 	"github.com/orsinium-labs/wasm4go/w4"
 )
 
+// The length of a side of one snake segment, in pixels.
 const size = 8
 
 var (
-	snake       = &Snake{}
-	frameCount  = 0
-	fruit       = w4.Point{X: 80, Y: 80}
+	snake = Snake{}
+
+	// The global counter of frames. Used to skip some because wasm-4 has 60 FPS
+	// but that's too fast.
+	frameCount = 0
+
+	// The fruit position. Place the first one in the center of the screen.
+	fruit = w4.Point{X: 80, Y: 80}
+
 	fruitSprite = []byte{0x00, 0xa0, 0x02, 0x00, 0x0e, 0xf0, 0x36, 0x5c, 0xd6, 0x57, 0xd5, 0x57, 0x35, 0x5c, 0x0f, 0xf0}
 	randInt     = rand.New(rand.NewSource(1)).Intn
 )
@@ -38,27 +45,28 @@ func input() {
 }
 
 func start() {
+	// https://wasm4.org/docs/tutorials/snake/setting-color-palette
 	w4.Palette.Set(
+		// White color for the background.
 		w4.Color{R: 0xfb, G: 0xf7, B: 0xf3},
+		// A soft orange color for the fruit body.
 		w4.Color{R: 0xe5, G: 0xb0, B: 0x83},
+		// Dark green color for the snake's segments and fruit leaf.
 		w4.Color{R: 0x42, G: 0x6e, B: 0x5d},
+		// Dark blue color for the snake's body.
 		w4.Color{R: 0x20, G: 0x28, B: 0x3d},
 	)
 	snake.Reset()
 }
 
 func update() {
-	frameCount++
-
 	input()
-
+	frameCount++
 	if frameCount%10 == 0 {
 		snake.Update()
-
 		if snake.IsDead() {
 			snake.Reset()
 		}
-
 		if snake.Body[0] == fruit {
 			snake.Body = append(snake.Body, snake.Body[len(snake.Body)-1])
 			fruit.X = uint8(randInt(20) * size)
@@ -66,7 +74,6 @@ func update() {
 		}
 	}
 	snake.Draw()
-
 	w4.DrawColors.Set(w4.Light, w4.Primary, w4.Secondary, w4.Dark)
 	w4.Blit(fruitSprite, fruit, w4.Size{Width: size, Height: size}, w4.TwoBPP)
 }
@@ -111,10 +118,11 @@ func (s *Snake) Update() {
 
 	s.Body[0].X = uint8((int(s.Body[0].X) + s.Direction.X) % 160)
 	s.Body[0].Y = uint8((int(s.Body[0].Y) + s.Direction.Y) % 160)
-	if s.Body[0].X < 0 {
+	// It is more than 160 if the integer overflows
+	if s.Body[0].X > 160 {
 		s.Body[0].X = 160 - size
 	}
-	if s.Body[0].Y < 0 {
+	if s.Body[0].Y > 160 {
 		s.Body[0].Y = 160 - size
 	}
 }
